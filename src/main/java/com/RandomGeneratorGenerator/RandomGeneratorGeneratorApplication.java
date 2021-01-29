82540fe50542bc131ed60962741d4b1baeef4840
Nights-Wolf <69758484+Nights-Wolf@users.noreply.github.com>
2021-01-29 12:29:49 +0100

    Test db and gui "success"

--- src/main/java/com/RandomGeneratorGenerator/RandomGeneratorGeneratorApplication.java
+++ src/main/java/com/RandomGeneratorGenerator/RandomGeneratorGeneratorApplication.java
@@ -1,18 +1,43 @@
 package com.RandomGeneratorGenerator;
 
+
+import lombok.Getter;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+import java.awt.*;
 
 @SpringBootApplication
+@Getter
 public class RandomGeneratorGeneratorApplication implements CommandLineRunner {
 
+	private final JButton saveButton = new JButton("Save");
+
 	public static void main(String[] args) {
-		SpringApplication.run(RandomGeneratorGeneratorApplication.class, args);
+
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(RandomGeneratorGeneratorApplication.class);
+		builder.headless(false);
+		ConfigurableApplicationContext context = builder.run(args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		SwingUtilities.invokeLater(() -> {
 
-	}
+					JFrame mainFrame = new JFrame("Main Frame");
+
+					mainFrame.setSize(500, 350);
+					mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					mainFrame.setVisible(true);
+
+					JPanel pan = new JPanel();
+					pan.setLayout(new FlowLayout(FlowLayout.CENTER));
+					mainFrame.add(pan);
+
+					mainFrame.add(saveButton);
+			});
+		}
 }
--- src/test/java/com/RandomGeneratorGenerator/RandomGeneratorGeneratorApplicationTests.java
+++ src/test/java/com/RandomGeneratorGenerator/RandomGeneratorGeneratorApplicationTests.java
@@ -1,10 +1,19 @@
 package com.RandomGeneratorGenerator;
 
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class RandomGeneratorGeneratorApplicationTests {
+
+	@BeforeClass
+	public static void setupHeadlessMode() {
+		System.setProperty("java.awt.headless", "false");
+	}
 
 	@Test
 	void contextLoads() {
